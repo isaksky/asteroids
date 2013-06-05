bef9e92a36a537a2e763599e7dde366a7bd19233
Isak Sky <isak.sky@gmail.com>
2013-06-04 21:39:53 -0700

    middleman - use asset hash

--- config.rb
+++ config.rb
@@ -60,7 +60,7 @@ configure :build do
   #activate :minify_css
   activate :minify_javascript
 
-  #activate :asset_hash
+  activate :asset_hash
 
   # Enable cache buster
   # activate :cache_buster
