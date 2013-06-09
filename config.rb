4fa376998e5cb3515f8066e00223e8592772f213
Isak Sky <isak.sky@gmail.com>
2013-06-09 10:58:03 -0700

    turn off js breakifier

--- config.rb
+++ config.rb
@@ -58,7 +58,7 @@ activate :livereload
 # Build-specific configuration
 configure :build do
   #activate :minify_css
-  activate :minify_javascript
+  #activate :minify_javascript
 
   activate :asset_hash
 
